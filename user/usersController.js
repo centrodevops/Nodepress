21154350a73223ad3df6b55d5654eda5ba81cc49
Lucas Levi <74688555+olucaslevi@users.noreply.github.com>
2022-07-29 19:36:47 -0300

    29-07-2022 -> Begin made user forms login and
    ---------------------
    user password hashable: ok
    user CRUD: 50%
    .css files with EJS files: ok
    mysql models connection: ok
    navbar bugs fixed.
    ---------------------

--- user/usersController.js
+++ user/usersController.js
@@ -2,28 +2,76 @@ const express = require('express');
 const router = express.Router(); 
 const User = require('./User.js'); 
 
-router.get("/admin/users",(req,res)=>{
-    res.send("Lista de Users")
-    }
-    );
+// ! Usando Hashs para criptografar a senha
+const bcrypt = require('bcrypt');
 
+
+router.get("/admin/users",(req,res)=>{ // Rota para listar todos os usuarios
+    User.findAll().then(users=>{
+        res.render("admin/users/index",{users: users}); // Renderiza o index.ejs e passa um json de usuarios
+    }
+    ).catch(err=>{
+        console.log(err);
+    }
+    )
+}
+)
 router.get("/admin/users/create",(req,res)=>{
         res.render("admin/users/create.ejs");
     }
     );
 router.post("/admin/users/create",(req,res)=>{
-    const {name,email,password} = req.body;
-    User.create({
-        name: name,
-        email: email,
-        password: password
-    }).then(user=>{
-        res.redirect("/admin/users");
-    }).catch(err=>{
-        console.log(err);
-    }
-    )
+    const {email,password} = req.body;
 
+    // ! Antes de gerar o hash, verificar se ESSE EMAIL já foi cadastrado no banco de dados.
+    User.findOne({where:{email:email}}).then(user=>{
+        if(user){
+            res.redirect("/admin/users/create");
+            console.log('Email já cadastrado');
+        }else{
+            // ! Gerar o hash da senha
+            bcrypt.hash(password,9).then(hash=>{
+                // ! Criar o usuário
+                User.create({
+                    email:email,
+                    password:hash
+                }).then(user=>{
+                    console.log(user);
+                    res.redirect("/admin/users");
+                }).catch(err=>{
+                    res.redirect("/admin/users/create");
+                    console.log(err);
+                }
+                );
+            }
+            );
+        }
+    }
+    );
 }
 );
+
+router.post("/admin/users/delete" , (req,res)=>{
+    var idForDelete = req.body.id;
+    if(idForDelete != undefined){
+        if (!isNaN(idForDelete)){
+            User.destroy({
+                where: {
+                    id:idForDelete
+                }
+                    
+            }).then(()=>{
+                console.log("Deletado com sucesso");
+                res.redirect("/admin/categories")
+            })
+        }else{
+            console.log("Não é um número");
+            res.redirect("/admin/categories");
+        }
+    }else{
+        console.log("Não existe");
+        res.redirect("/admin/categories");
+    }
+})
+
 module.exports = router;
