3385e68fb827f38323d85b8ebbfb04967a13613c
Lucas Levi <74688555+olucaslevi@users.noreply.github.com>
2023-03-03 11:57:03 -0300

    reset status commit

--- Database/database.js
+++ Database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 // Indica onde estamos nos conectando
-const connection = new Sequelize('wordpress_clone','root','MY-PASS',{
+const connection = new Sequelize('wordpress_clone','MY-USER','MY-PASS',{
     //parametros
     host: 'localhost',
     dialect: 'mysql',
